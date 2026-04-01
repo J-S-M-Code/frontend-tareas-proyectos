151876f77f8028b5ccacc9f11e2cca17642d6cc4
J-S-M-Code <joacosuilar@gmail.com>
2026-04-01 11:38:47 -0300

    SPEC-createTask

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { CreateProjectComponent } from './feature/projects/create-project/create-project.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Cuando la ruta está vacía (localhost:4200/), carga el componente de crear
+  {
+    path: '',
+    component: CreateProjectComponent,
+  },
+  // Opcional: dejamos la ruta explícita armada para el futuro
+  {
+    path: 'proyectos/crear',
+    component: CreateProjectComponent,
+  },
+  // Si el usuario ingresa una URL que no existe, lo mandamos a la principal
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
