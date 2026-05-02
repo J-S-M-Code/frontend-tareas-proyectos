cd5a87d430f9c38036e87684412d1dc6db2d7502
J-S-M-Code <joacosuilar@gmail.com>
2026-05-02 08:49:43 -0300

    SPEC and estructure

---
+++ src/app/feature/add-comment/add-comment.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-comment',
+  imports: [],
+  templateUrl: './add-comment.html',
+  styleUrl: './add-comment.css',
+})
+export class AddComment {}
